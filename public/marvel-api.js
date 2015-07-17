3344edde01bb3741a57c93a1225e27dead7e9988
jaime feldman <jaimefel@gmail.com>
2015-07-16 22:54:15 -0300

    agregando la funcion localstorage a marvel-api.js para guardar y recuperar datos de la cache del navegador.

--- public/marvel-api.js
+++ public/marvel-api.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var $ = window.jQuery;
 
@@ -11,23 +11,44 @@ var MarvelApi = (function () {
 		_classCallCheck(this, MarvelApi);
 
 		this.key = key;
-		this.baseURL = 'http://gateway.marvel.com:80/v1/public/';
+		this.baseURL = "http://gateway.marvel.com:80/v1/public/";
 	}
 
 	_createClass(MarvelApi, [{
-		key: 'findSeries',
+		key: "findSeries",
 		value: function findSeries(title) {
-			var url = this.baseURL + 'series?title=' + title + '&apikey=' + this.key;
-			return Promise.resolve($.get(url)).then(function (result) {
-				return result.data.results[0];
+			var url = this.baseURL + "series?title=" + title + "&apikey=" + this.key;
+			if (localStorage[url]) {
+				var datos = localStorage[url];
+				datos = JSON.parse(datos);
+				console.log("obteniendo los datos de la cache del navegador : " + datos);
+				return Promise.resolve(datos);
+			}
+			return Promise.resolve($.get(url)).then(function (res) {
+				var datos = res.data.results[0];
+				datos = JSON.stringify(datos);
+				localStorage[url] = datos;
+				console.log("obteniendo datos de la url y almacenandolos en la cache" + datos);
+				return Promise.resolve(datos);
 			});
 		}
 	}, {
-		key: 'getResourceURI',
+		key: "getResourceURI",
 		value: function getResourceURI(resouceURI) {
-			var url = resouceURI + '?apikey=' + this.key;
+			var url = resouceURI + "?apikey=" + this.key;
+			if (localStorage[url]) {
+				var datos = localStorage[url];
+				datos = JSON.parse(datos);
+				console.log("obteniendo datos de la cache del navegaror..");
+				return Promise.resolve(datos);
+			}
+
 			return Promise.resolve($.get(url)).then(function (res) {
-				return res.data.results[0];
+				var datos = res.data.results[0];
+				datos = JSON.stringify(datos);
+				localStorage[url] = datos;
+				console.log("obteniendo los datos de la url de marvel y almacenandolos en la cache.. ");
+				return Promise.resolve(datos);
 			});
 		}
 	}]);
